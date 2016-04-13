bfd0b7e2495e0b5de0880982737ab385e5a76b40
snkrt <snkrt075@gmail.com>
2016-04-13 16:23:26 +0800

    app2

--- project01/src/main/java/com/number2/www/project01/App.java
+++ project01/src/main/java/com/number2/www/project01/App.java
@@ -9,11 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-    	System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "上海自來水來自水上" );
-
-        System.out.println( "0000000000" );     
+       
 
 
 
