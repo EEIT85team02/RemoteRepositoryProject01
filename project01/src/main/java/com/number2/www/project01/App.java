c091300ff27acfcf1e43b0434ca26286361c5581
a25251226 <a25251226@gmail.com>
2016-04-13 15:56:34 +0800

    hahahaha222

--- project01/src/main/java/com/number2/www/project01/App.java
+++ project01/src/main/java/com/number2/www/project01/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hahaha");
         System.out.println("hahaha111");
+        System.out.println("hahaha222");
     }
 }
